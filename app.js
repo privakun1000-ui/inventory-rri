e1d914c38531cda3a6c98684af475775185b124f
privakun1000-ui <privakun1000@gmail.com>
2026-06-15 10:57:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const API_URL='https://script.google.com/macros/s/AKfycbw7u1xSxfG8AWNYrw0zp22gGIzFfPdnVWHzOD_lc9tzqojzi4K8uJXEKgQfzrSnogXzsA/exec';
 
 async function cekApi(){
+
  const status=document.getElementById('status');
- status.className='alert alert-warning';
- status.innerHTML='Menghubungi server...';
 
  try{
-   const response=await fetch(API_URL);
-   const data=await response.json();
 
-   status.className='alert alert-success';
-   status.innerHTML=`<strong>Server Aktif</strong><br>App : ${data.app||'-'}<br>Version : ${data.version||'-'}`;
+   const response = await fetch(API_URL);
+
+   const text = await response.text();
+
+   console.log(text);
+
+   status.innerHTML =
+     '<pre>'+text+'</pre>';
+
  }catch(error){
-   status.className='alert alert-danger';
-   status.innerHTML='Gagal terhubung ke Apps Script<br>'+error;
+
+   console.error(error);
+
+   status.innerHTML =
+     'ERROR : '+error;
+
  }
+
 }
 
 window.onload=cekApi;
